9030cbd0d72a99d11b037935ba24af60e9da5bc9
lokeshIIITB <lglg798@gmail.com>
2019-01-17 11:38:51 +0530

    multiplication added

--- demo.cpp
+++ demo.cpp
@@ -2,9 +2,7 @@
 using namespace std;
 int main()
 {
-	cout<<"Enter any two numbers\n";
-	int a,b;
-	cin>>a>>b;
-	cout<<"addition of two numbers = "<<a+b<<endl;
+	cout<<"multiplication of two numbers = "<<a*b<<endl;
+	//here we assume a and b present in main
 	return 0;
 }
