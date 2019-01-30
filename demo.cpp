536fef1ead6567b68f71dbc8b391165df24a5b26
lokesg garg <lglg798@gmail.com>
2019-01-30 18:43:52 +0530

    difference added

--- demo.cpp
+++ demo.cpp
@@ -5,6 +5,7 @@ int main()
 	cout<<"Enter any two numbers\n";
 	int a,b;
 	cin>>a>>b;
+	cout<<"absolute difference = "<<abs(a-b)<<endl;
 	cout<<"addition of two numbers = "<<a+b<<endl;
 	cout<<"multiplication of two numbers = "<<a*b<<endl;
 	//here we assume a and b present in main
